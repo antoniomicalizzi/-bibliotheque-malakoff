2ee81e868d27f39917f5cb2f19f26fcc6aabed22
antoniomicalizzi <micalizzi.anto@gmail.com>
2021-07-06 18:01:25 +0200

    insertion carte

--- src/views/Info.jsx
+++ src/views/Info.jsx
@@ -8,6 +8,23 @@ function Info() {
     <div>
       <Navigation />
       <h1>Info</h1>
+      <div className="noustrouver">
+        <h3> Heures d’ouverture au public en accès libre </h3>
+          <ul>
+            <li> Mardi 12h-13h et 16h-18h</li>
+            <li>Mercredi 14h-18h30</li>
+            <li> Jeudi et Vendredi 16h-18h</li>
+            <li>Samedi 10h-12h</li>
+          </ul>
+        <h3>Entrée libre</h3>
+          <p> Toute lecture ou consultation sur place est libre et gratuite, sans limitation de durée, aussi souvent que vous le souhaitez.</p>
+        <h3> Transports</h3>
+          <p> Se rendre à l’Espace Lecture en transport en commun avec le C3 et bus ligne 5 
+          (lien du site de la TAN) </p>
+      </div>
+      <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10840.075965940747!2d-1.5250113!3d47.2162107!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x30cd131e61341202!2sBiblioth%C3%A8que%20Espace%20Lecture!5e0!3m2!1sfr!2sfr!4v1625577461376!5m2!1sfr!2sfr" title="map" width="600" height="450" allowfullscreen="" loading="lazy"></iframe>
+      </div>
       <Faq />
       <Footer />
     </div>

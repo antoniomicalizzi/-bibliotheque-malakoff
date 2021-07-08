30ef4880d9c27a33701b1455b1f5a803693c55fc
antoniomicalizzi <micalizzi.anto@gmail.com>
2021-07-08 22:58:04 +0200

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,11 +1,18 @@
-import affiche from '../assets/affiche.png'
 import './Footer.css';
+import logo from '../assets/Nantes_logo.png';
 
 function Footer() {
   return (
-    <div className="footer">
-      <img className="affiche" src={affiche} alt="affiche" />
+  <div className="footer">
+    <div className="espace">
+     <h1>Espace Lecture</h1>
+     <p>3-5 rue d’Autriche – 44000 Nantes<br />tél. 07 49 50 75 48 <br />mail bibliotheque@espacelecture.fr</p>
     </div>
+    <div className="logo">
+      <p>en collaboration avec</p>
+      <img className="logo-ville" src={logo} alt="logo-ville" />
+    </div>
+  </div>
   )
 }
 

d2401135a96b6cd29fe9824e2700fdcdd326a3f6
antoniomicalizzi <micalizzi.anto@gmail.com>
2021-08-31 11:04:18 +0200

    page en construction

--- src/views/Projets.jsx
+++ src/views/Projets.jsx
@@ -1,10 +1,12 @@
 import Footer from "../layout/Footer"
 import Navigation from"../layout/Navigation"
 
+import './Projets.css';
+
 function Projets() {
   return <div>
     <Navigation />
-    <h1>Projets</h1>
+    <h1 className="PagesToCome">&#x26A0; Page en Construction &#x26A0;</h1>
     <Footer />
   </div>;
 }
--- src/views/VieAsso.jsx
+++ src/views/VieAsso.jsx
@@ -5,7 +5,7 @@ function VieAsso() {
   return (
     <div>
       <Navigation />
-      <h1>Vie Associative</h1>
+      <h1 className="PagesToCome">&#x26A0; Page en Construction &#x26A0;</h1>
       <Footer />
     </div>
   );
